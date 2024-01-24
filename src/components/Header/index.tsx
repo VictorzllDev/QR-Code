b40b6be2614c479709406b96b2bf053f9fdba62a
Victorzlldev <joaovictor12200capa@gmail.com>
2024-01-24 17:40:45 -0300

    SearchParams in input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { QrCode } from 'lucide-react'
 
-export default function Header() {
+export default function Header(): JSX.Element {
   return (
     <header className="absolute top-0 flex h-16 w-full flex-row items-center justify-center bg-zinc-900 px-3 py-2">
       <QrCode size={32} />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,25 @@
+import { useSearchParams } from 'react-router-dom'
+
 export default function Home(): JSX.Element {
+  let [searchParams, setSearchParams] = useSearchParams()
+
+  let url: string | null = searchParams.get('url')
+
   return (
     <main className="flex min-h-screen items-center justify-center pt-16">
-      <form className="bg-secondary space-y-4 rounded-lg p-4 shadow-2xl shadow-zinc-900/40">
+      <form className="space-y-4 rounded-lg bg-secondary p-4 shadow-2xl shadow-zinc-900/40">
         <input
           type="text"
           placeholder="https://google.com"
-          className="focus:border-primary h-14 w-full rounded border-2 px-3 text-black outline-none duration-300 focus:border-2"
+          defaultValue={url ?? ''}
+          onChange={(e) => setSearchParams({ url: e.target.value })}
+          className="h-14 w-full rounded border-2 px-3 text-black outline-none duration-300 focus:border-2 focus:border-primary"
         />
         <img
-          src="/src/assets/killuaLoad.gif"
-          className="h-72 w-72 rounded object-cover object-center"
+          src=""
+          className="h-auto w-72 rounded object-cover object-center"
         />
-        <button className="bg-primary/80 hover:bg-primary h-14 w-full rounded duration-300">
+        <button className="h-14 w-full rounded bg-primary/80 duration-300 hover:bg-primary">
           Gerar QR-Code
         </button>
       </form>
