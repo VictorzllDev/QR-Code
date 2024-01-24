28efe081b7bfbaa7986c4731a39a244a8674e075
Victorzlldev <joaovictor12200capa@gmail.com>
2024-01-24 17:40:06 -0300

    Error Page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,7 +1,17 @@
+import { LucideMapPinOff } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
 export default function ErrorPage(): JSX.Element {
   return (
-    <main>
-      <h1>Not Found</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <LucideMapPinOff size={62} />
+      <h1 className="text-5xl font-bold">Page Not Found</h1>
+      <Link
+        to="/"
+        className="bg-primary/80 hover:bg-primary mt-12 rounded px-6 py-2 text-lg duration-300"
+      >
+        Voltar
+      </Link>
     </main>
   )
 }
