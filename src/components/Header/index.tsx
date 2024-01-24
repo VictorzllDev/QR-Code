231c9cf5ff59c0a5211cc2ad0d6beb2650fdfbdf
Victorzlldev <joaovictor12200capa@gmail.com>
2024-01-24 15:58:07 -0300

    Header, Card create

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { QrCode } from 'lucide-react'
+
+export default function Header() {
+  return (
+    <header className="absolute top-0 flex h-16 w-full flex-row items-center justify-center bg-zinc-900 px-3 py-2">
+      <QrCode size={32} />
+    </header>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#A60F2B',
+        secondary: '#2F2340',
+        tertiary: '#131626',
+        quarteira: '#8AA696',
+        quiteria: '#F2D6A2',
+      },
+    },
   },
   plugins: [],
 }
-
